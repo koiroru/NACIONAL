c6ce45e906546c382a532856cd4c5ba9633b8e38
Canelita <ing69chipmunks@hotmail.com>
2023-11-27 16:33:05 -0300

    LET'S GOOO

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,12 +31,20 @@ android {
     }
 }
 
+
+
+
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("androidx.appcompat:appcompat:1.3.1")
+    /*implementation 'androidx.constraintlayout:constraintlayout:2.1.1'
+    testImplementation 'junit:junit:4.13.2'
+    androidTestImplementation 'androidx.test.ext:junit:1.1.3'
+    androidTestImplementation 'androidx.test.espresso:espresso-core:3.4.0'
+    implementation 'androidx.localbroadcastmanager:localbroadcastmanager:1.1.0'
+    */
+    implementation ("androidx.appcompat:appcompat:1.3.1")
+    implementation ("com.google.android.material:material:1.4.0")
+    implementation ("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+    implementation ("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
 }
--- app/src/main/java/com/example/nacional/MainActivity.java
+++ app/src/main/java/com/example/nacional/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.nacional;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Mqtt mqttManager;
+    EditText Nombre;
+    EditText Director;
+    EditText Sipnosis;
+    EditText Opinion;
+    EditText Calificacion;
+    Button Agregar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Nombre = findViewById(R.id.agreganombre);
+        Director = findViewById(R.id.agregadirector);
+        Sipnosis = findViewById(R.id.agregasipnosis);
+        Opinion = findViewById(R.id.agregaopinion);
+        Calificacion = findViewById(R.id.agregacalificacion);
+        Agregar = findViewById(R.id.btncalificar);
+
+        mqttManager = new Mqtt(getApplicationContext());
+        mqttManager.connectToMqttBroker();
+
+        Agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    mqttManager.publishMessage(Nombre.getText().toString());
+                    mqttManager.publishMessage(Director.getText().toString());
+                    mqttManager.publishMessage(Sipnosis.getText().toString());
+                    mqttManager.publishMessage(Opinion.getText().toString());
+                    mqttManager.publishMessage(Calificacion.getText().toString());
+                    }
+            });
     }
 }
